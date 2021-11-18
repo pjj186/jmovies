0a58473349027b2bc98b010de43cdda0b4cbffda
Jong jae Park <pjj186@naver.com>
2021-11-18 18:20:54 +0900

    use Hooks
    AppLoading을 할때, API호출이나 데이터베이스 초기화 등 다른 작업이 아닌 오직 폰트들이나 에셋들을 프리로드 하는 작업만 한다면
    hook을 이용하여 처리하는것이 더 나은 방법이다.
    물론 여러 작업을 처리할 땐 로딩 함수를 만들어 처리하는것이 좋다.

--- App.js
+++ App.js
@@ -3,42 +3,14 @@ import React, { useState } from "react";
 import { Text, Image } from "react-native";
 import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
-import { Asset } from "expo-asset";
-
-const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
-
-const loadImages = (images) =>
-  images.map((image) => {
-    if (typeof image === "string") {
-      return Image.prefetch(image);
-    } else {
-      return Asset.loadAsync(image);
-    }
-  });
+import { Asset, useAssets } from "expo-asset";
 
 export default function App() {
-  const [ready, setReady] = useState(false);
-  const onFinish = () => setReady(true);
-  const startLoading = async () => {
-    // 로딩하고 싶은 코드들을 담는곳
+  const [asset] = useAssets([require("./my_profile.jpeg")]);
+  const [loaded] = Font.useFonts(Ionicons.font);
 
-    // Promise들의 배열
-    const fonts = loadFonts([Ionicons.font]);
-    const images = loadImages([
-      require("./my_profile.jpeg"),
-      "https://t1.daumcdn.net/cfile/tistory/24457C4F58663DD011",
-    ]);
-
-    await Promise.all([...fonts, ...images]);
-  };
-  if (!ready) {
-    return (
-      <AppLoading
-        startAsync={startLoading}
-        onFinish={onFinish}
-        onError={console.error}
-      />
-    );
+  if (!asset || !loaded) {
+    return <AppLoading />;
   }
   return <Text>We are done loading</Text>;
 }
