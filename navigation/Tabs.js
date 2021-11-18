0dc283a84b394e69c1e8180926471afb96ed7ee5
Jong jae Park <pjj186@naver.com>
2021-11-18 19:53:11 +0900

    Configuring the Navigation
    screenOption은 모든 스크린에 주는 옵션,
    각각 주고싶다면 각 스크린에 option prop으로 줄 수 있다.

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { View, Text } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
@@ -7,8 +8,29 @@ import Search from "../screens/Search";
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => (
-  <Tab.Navigator>
-    <Tab.Screen name="Movies" component={Movies}></Tab.Screen>
+  <Tab.Navigator
+    screenOptions={{
+      tabBarStyle: {
+        backgroundColor: "tomato",
+      },
+      tabBarActiveTintColor: "red",
+      tabBarInactiveTintColor: "purple",
+    }}
+  >
+    <Tab.Screen
+      name="Movies"
+      component={Movies}
+      options={{
+        headerTitleStyle: {
+          color: "tomato",
+        },
+        headerRight: () => (
+          <View>
+            <Text>Hello</Text>
+          </View>
+        ),
+      }}
+    ></Tab.Screen>
     <Tab.Screen name="Tv" component={Tv}></Tab.Screen>
     <Tab.Screen name="Search" component={Search}></Tab.Screen>
   </Tab.Navigator>
