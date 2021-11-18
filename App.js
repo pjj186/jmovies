c348cc598745a2e95a607b8945729d82857190bb
Jong jae Park <pjj186@naver.com>
2021-11-18 18:18:58 +0900

    로딩 함수 개편
    여러개의 폰트나, 에셋을 프리로드 하는 경우를 생각하여, 배열을 인자로 받는 loadFonts 함수와 loadImages 함수를 정의하고 각 함수들은 Promise를 배열의 형태로 리턴한다.
    그러면 그 Promise들을 Promise.all 함수를 이용하여 처리해준다.

--- App.js
+++ App.js
@@ -5,16 +5,31 @@ import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
 import { Asset } from "expo-asset";
 
+const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+
+const loadImages = (images) =>
+  images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.loadAsync(image);
+    }
+  });
+
 export default function App() {
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true);
   const startLoading = async () => {
     // 로딩하고 싶은 코드들을 담는곳
-    await Font.loadAsync(Ionicons.font);
-    await Asset.loadAsync(require("./my_profile.jpeg"));
-    await Image.prefetch(
-      "https://t1.daumcdn.net/cfile/tistory/24457C4F58663DD011"
-    );
+
+    // Promise들의 배열
+    const fonts = loadFonts([Ionicons.font]);
+    const images = loadImages([
+      require("./my_profile.jpeg"),
+      "https://t1.daumcdn.net/cfile/tistory/24457C4F58663DD011",
+    ]);
+
+    await Promise.all([...fonts, ...images]);
   };
   if (!ready) {
     return (
