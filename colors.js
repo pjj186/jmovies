aa6096186544fc3cef1a7880f9fedf20c22e9d19
Jong jae Park <pjj186@naver.com>
2021-11-18 20:23:45 +0900

    다크 모드 / 색상파일 추가

---
+++ colors.js
@@ -0,0 +1,3 @@
+export const YELLOW_COLOR = "#eccc68";
+export const BLACK_COLOR = "#2f3542";
+export const GREY_COLOR = "#747d8c";
--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -4,36 +4,36 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
 import Search from "../screens/Search";
+import { useColorScheme } from "react-native";
+import { BLACK_COLOR, GREY_COLOR, YELLOW_COLOR } from "../colors";
 
 const Tab = createBottomTabNavigator();
 
-const Tabs = () => (
-  <Tab.Navigator
-    screenOptions={{
-      tabBarStyle: {
-        backgroundColor: "tomato",
-      },
-      tabBarActiveTintColor: "red",
-      tabBarInactiveTintColor: "purple",
-    }}
-  >
-    <Tab.Screen
-      name="Movies"
-      component={Movies}
-      options={{
-        headerTitleStyle: {
-          color: "tomato",
+const Tabs = () => {
+  // useColorScheme : "dark"인지 "light"인지 colorScheme을 알려주는 Hook
+  // useColorScheme은 "dark" 또는 "light"라는 문자열을 리턴하는데, 이를 이용하여 isDark 변수에 true / false 값이 저장되게함
+  const isDark = useColorScheme() === "dark";
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: isDark ? BLACK_COLOR : "white",
         },
-        headerRight: () => (
-          <View>
-            <Text>Hello</Text>
-          </View>
-        ),
+        tabBarActiveTintColor: isDark ? YELLOW_COLOR : BLACK_COLOR,
+        tabBarInactiveTintColor: isDark ? GREY_COLOR : GREY_COLOR,
+        headerStyle: {
+          backgroundColor: isDark ? BLACK_COLOR : "white",
+        },
+        headerTitleStyle: {
+          color: isDark ? "white" : BLACK_COLOR,
+        },
       }}
-    ></Tab.Screen>
-    <Tab.Screen name="Tv" component={Tv}></Tab.Screen>
-    <Tab.Screen name="Search" component={Search}></Tab.Screen>
-  </Tab.Navigator>
-);
+    >
+      <Tab.Screen name="Movies" component={Movies}></Tab.Screen>
+      <Tab.Screen name="Tv" component={Tv}></Tab.Screen>
+      <Tab.Screen name="Search" component={Search}></Tab.Screen>
+    </Tab.Navigator>
+  );
+};
 
 export default Tabs;
