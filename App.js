5aca25d2d44394c2c292efd7f445bbd9c706e5d4
Jong jae Park <pjj186@naver.com>
2021-11-18 17:20:21 +0900

    AppLoading part Two

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import AppLoading from "expo-app-loading";
 import React, { useState } from "react";
-import { Text } from "react-native";
+import { Text, Image } from "react-native";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import { Asset } from "expo-asset";
 
 export default function App() {
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true);
   const startLoading = async () => {
-    await new Promise((resolve) => setTimeout(resolve, 10000));
+    // 로딩하고 싶은 코드들을 담는곳
+    await Font.loadAsync(Ionicons.font);
+    await Asset.loadAsync(require("./my_profile.jpeg"));
+    await Image.prefetch(
+      "https://t1.daumcdn.net/cfile/tistory/24457C4F58663DD011"
+    );
   };
   if (!ready) {
     return (
