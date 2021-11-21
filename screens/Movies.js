4a7741bc9e81af378eda101e7e5bcc7b118a5a8e
Jong jae Park <pjj186@naver.com>
2021-11-22 03:09:37 +0900

    Update Movies.js

--- screens/Movies.js
+++ screens/Movies.js
@@ -4,7 +4,7 @@ import { TouchableOpacity, Text } from "react-native";
 const Movies = ({ navigation: { navigate } }) => (
   <TouchableOpacity
     // 서로 다른 Navigator 사이를 이동할때는 navigate("스크린이름") 방식으로 하면 작동하지 않는다.
-    // navigate("navigator name", {screen: "스크린 이름"}) 이런식으로 해야한다.
+    // navigate("navigator이름"(Root.js에 명시되있음), {screen: "스크린 이름"}) 이런식으로 해야한다.
     onPress={() => navigate("Stack", { screen: "Three" })}
     style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
   >
