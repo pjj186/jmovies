6e308a9eea2de39f652b4be3cf13151c3aaf79f3
Jong jae Park <pjj186@naver.com>
2021-11-23 00:35:37 +0900

    타입스크립트 적용
    타입스크립트 적용, 기본적인 타입스크립트에 대해 공부해야겠다.

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,3 +1,4 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
 import styled from "styled-components/native";
 
@@ -12,7 +13,9 @@ const Title = styled.Text`
   color: ${(props) => props.theme.textColor};
 `;
 
-const Movies = ({ navigation: { navigate } }) => (
+const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = ({
+  navigation: { navigate },
+}) => (
   <Btn
     // 서로 다른 Navigator 사이를 이동할때는 navigate("스크린이름") 방식으로 하면 작동하지 않는다.
     // navigate("navigator이름"(Root.js에 명시되있음), {screen: "스크린 이름"}) 이런식으로 해야한다.
