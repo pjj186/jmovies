da5599ded967ac18414f94b8debbbd7e4abf0820
Jong jae Park <pjj186@naver.com>
2021-11-22 17:41:35 +0900

    Styled Components
    리액트 네이티브도 styled-components를 사용 할 수 있음!

--- screens/Movies.js
+++ screens/Movies.js
@@ -1,15 +1,25 @@
 import React from "react";
-import { TouchableOpacity, Text } from "react-native";
+import styled from "styled-components/native";
+
+const Btn = styled.TouchableOpacity`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.Text`
+  color: ${(props) => (props.selected ? "blue" : "red")};
+`;
 
 const Movies = ({ navigation: { navigate } }) => (
-  <TouchableOpacity
+  <Btn
     // 서로 다른 Navigator 사이를 이동할때는 navigate("스크린이름") 방식으로 하면 작동하지 않는다.
     // navigate("navigator이름"(Root.js에 명시되있음), {screen: "스크린 이름"}) 이런식으로 해야한다.
     onPress={() => navigate("Stack", { screen: "Three" })}
-    style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
   >
-    <Text>Movie</Text>
-  </TouchableOpacity>
+    <Title selected={false}>Movie</Title>
+    <Title selected={true}>Movie</Title>
+  </Btn>
 );
 
 export default Movies;
