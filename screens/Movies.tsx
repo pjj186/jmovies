31590058bf6c6fce6cd215476f0ec2a5a2b4a948
Jong jae Park <pjj186@naver.com>
2021-11-23 11:29:34 +0900

    Movie Screen Part One (Swiper)
    Swiper 구현

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,9 +1,46 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
+import { Dimensions } from "react-native";
 import styled from "styled-components/native";
+import Swiper from "react-native-web-swiper";
 
-const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = ({
-  navigation: { navigate },
-}) => null;
+// Request URL
+
+const API_KEY = "0c6f3c84d4564ed4f2afcb7aa3744089";
+
+const Container = styled.ScrollView`
+  background-color: ${(props) => props.theme.mainBgColor};
+`;
+
+const View = styled.View`
+  flex: 1;
+`;
+
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
+
+const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
+  const getNowPlaying = () => {
+    // URL fetch function
+    const response = fetch(
+      `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=ko-KR&page=1&region=KR`
+    );
+  };
+
+  return (
+    <Container>
+      <Swiper
+        loop
+        timeout={3.5}
+        controlsEnabled={false}
+        containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
+      >
+        <View style={{ backgroundColor: "red" }}></View>
+        <View style={{ backgroundColor: "blue" }}></View>
+        <View style={{ backgroundColor: "red" }}></View>
+        <View style={{ backgroundColor: "blue" }}></View>
+      </Swiper>
+    </Container>
+  );
+};
 
 export default Movies;
