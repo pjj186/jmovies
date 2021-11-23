086bce5bf5f8a1c86397109826d0089942b43981
Jong jae Park <pjj186@naver.com>
2021-11-24 05:27:02 +0900

    리팩토링
    Slide 컴포넌트를 다른 파일에 따로 빼주었다.

---
+++ components/Slide.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+import styled from "styled-components/native";
+import { View, StyleSheet, useColorScheme } from "react-native";
+import { makeImagePath } from "../utils";
+import { BlurView } from "expo-blur";
+
+const BgImg = styled.Image``;
+
+const Poster = styled.Image`
+  width: 100px;
+  height: 160px;
+  border-radius: 5px;
+`;
+
+const Title = styled.Text<{ isDark: boolean }>`
+  font-size: 16px;
+  font-weight: 600;
+  color: ${(props) => (props.isDark ? "white" : props.theme.textColor)};
+`;
+
+const Wrapper = styled.View`
+  flex-direction: row;
+  height: 100%;
+  width: 90%;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Column = styled.View`
+  margin-left: 20px;
+  width: 60%;
+`;
+
+const Overview = styled.Text<{ isDark: boolean }>`
+  margin-top: 10px;
+  color: ${(props) =>
+    props.isDark ? "rgba(255, 255, 255, 0.8)" : "rgba(0, 0, 0, 0.8)"};
+`;
+
+// 스타일 상속받기
+const Votes = styled(Overview)`
+  font-size: 12px;
+`;
+
+interface SlideProps {
+  backdropPath: string;
+  posterPath: string;
+  voteAverage: number;
+  originalTitle: string;
+  overview: string;
+}
+
+// 타입스크립트를 곁들여 리액트 컴포넌트를 생성할 때의 형태
+// <> 사이에 매개변수들의 타입을 정해준 인터페이스를 넣음으로써 타입을 체킹
+const Slide: React.FC<SlideProps> = ({
+  backdropPath,
+  posterPath,
+  voteAverage,
+  originalTitle,
+  overview,
+}) => {
+  const isDark = useColorScheme() === "dark";
+
+  return (
+    <View style={{ flex: 1 }}>
+      <BgImg
+        style={StyleSheet.absoluteFill}
+        source={{ uri: makeImagePath(backdropPath) }}
+      />
+      <BlurView
+        tint={isDark ? "dark" : "light"}
+        intensity={85}
+        style={StyleSheet.absoluteFill}
+      >
+        <Wrapper>
+          <Poster source={{ uri: makeImagePath(posterPath) }} />
+          <Column>
+            <Title isDark={isDark}>{originalTitle}</Title>
+            {voteAverage > 0 ? (
+              <Votes isDark={isDark}>⭐️{voteAverage}/10</Votes>
+            ) : null}
+            <Overview isDark={isDark}>{overview.slice(0, 90)}...</Overview>
+          </Column>
+        </Wrapper>
+      </BlurView>
+    </View>
+  );
+};
+
+export default Slide;
--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,23 +1,13 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { useEffect, useState } from "react";
-import {
-  ActivityIndicator,
-  Dimensions,
-  StyleSheet,
-  useColorScheme,
-} from "react-native";
+import { ActivityIndicator, Dimensions } from "react-native";
 import styled from "styled-components/native";
 import Swiper from "react-native-swiper";
-import { makeImagePath } from "../utils";
-import { BlurView } from "expo-blur";
+import Slide from "../components/Slide";
 
 const API_KEY = "0c6f3c84d4564ed4f2afcb7aa3744089";
 
 const Container = styled.ScrollView``;
-
-const View = styled.View`
-  flex: 1;
-`;
 
 const Loader = styled.View`
   flex: 1;
@@ -25,46 +15,9 @@ const Loader = styled.View`
   align-items: center;
 `;
 
-const BgImg = styled.Image``;
-
-const Poster = styled.Image`
-  width: 100px;
-  height: 160px;
-  border-radius: 5px;
-`;
-
-const Title = styled.Text`
-  font-size: 16px;
-  font-weight: 600;
-  color: white;
-`;
-
-const Wrapper = styled.View`
-  flex-direction: row;
-  height: 100%;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Column = styled.View`
-  width: 40%;
-  margin-left: 15px;
-`;
-
-const Overview = styled.Text`
-  margin-top: 10px;
-  color: rgba(255, 255, 255, 0.8);
-`;
-
-// 스타일 상속받기
-const Votes = styled(Overview)`
-  font-size: 12px;
-`;
-
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
-  const isDark = useColorScheme() === "dark";
   const [loading, setLoading] = useState(true);
   const [nowPlaying, setNowPlaying] = useState([]);
 
@@ -99,28 +52,14 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
         containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
       >
         {nowPlaying.map((movie) => (
-          <View key={movie.id}>
-            <BgImg
-              style={StyleSheet.absoluteFill}
-              source={{ uri: makeImagePath(movie.backdrop_path) }}
-            />
-            <BlurView
-              tint={isDark ? "dark" : "light"}
-              intensity={80}
-              style={StyleSheet.absoluteFill}
-            >
-              <Wrapper>
-                <Poster source={{ uri: makeImagePath(movie.poster_path) }} />
-                <Column>
-                  <Title>{movie.original_title}</Title>
-                  {movie.vote_average > 0 ? (
-                    <Votes>⭐️{movie.vote_average}/10</Votes>
-                  ) : null}
-                  <Overview>{movie.overview.slice(0, 90)}...</Overview>
-                </Column>
-              </Wrapper>
-            </BlurView>
-          </View>
+          <Slide
+            key={movie.id}
+            backdropPath={movie.backdrop_path}
+            posterPath={movie.poster_path}
+            voteAverage={movie.vote_average}
+            originalTitle={movie.original_title}
+            overview={movie.overview}
+          />
         ))}
       </Swiper>
     </Container>
