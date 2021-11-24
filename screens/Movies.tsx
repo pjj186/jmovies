4aa0f1608eb84aee84683b024cd10e578f0aae9b
Jong jae Park <pjj186@naver.com>
2021-11-25 04:23:25 +0900

    Trending Movie
    트랜딩 무비를 수평 슬라이드뷰에 만듦

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,9 +1,10 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { useEffect, useState } from "react";
-import { ActivityIndicator, Dimensions } from "react-native";
+import { ActivityIndicator, Dimensions, useColorScheme } from "react-native";
 import styled from "styled-components/native";
 import Swiper from "react-native-swiper";
 import Slide from "../components/Slide";
+import Poster from "../components/Poster";
 
 const API_KEY = "0c6f3c84d4564ed4f2afcb7aa3744089";
 
@@ -17,16 +18,48 @@ const Loader = styled.View`
 
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
+const ListTitle = styled.Text<{ isDark: boolean }>`
+  font-size: 18px;
+  font-weight: 600;
+  margin-left: 30px;
+  color: ${(props) => (props.isDark ? "white" : props.theme.textColor)};
+`;
+
+const TrendingScroll = styled.ScrollView`
+  margin-top: 20px;
+`;
+
+const Movie = styled.View`
+  margin-right: 20px;
+  align-items: center;
+`;
+
+const Title = styled.Text<{ isDark: boolean }>`
+  font-weight: 600;
+  margin-top: 7px;
+  margin-bottom: 5px;
+  color: ${(props) =>
+    props.isDark ? "rgba(255, 255, 255, 0.8)" : "rgba(0, 0, 0, 0.8)"};
+`;
+
+const Votes = styled.Text<{ isDark: boolean }>`
+  font-size: 10px;
+  color: ${(props) =>
+    props.isDark ? "rgba(255, 255, 255, 0.8)" : "rgba(0, 0, 0, 0.8)"};
+`;
+
 const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
   const [loading, setLoading] = useState(true);
   const [nowPlaying, setNowPlaying] = useState([]);
   const [upcoming, setUpcoming] = useState([]);
   const [trending, setTrending] = useState([]);
 
+  const isDark = useColorScheme() === "dark";
+
   const getTrending = async () => {
     const { results } = await (
       await fetch(
-        `https://api.themoviedb.org/3/movie/trending/movie/week?api_key=${API_KEY}&language=ko-KR&page=1&region=KR`
+        `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}&language=ko-KR&page=1&region=KR`
       )
     ).json();
     setTrending(results);
@@ -70,7 +103,11 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
         autoplayTimeout={3.5}
         showsButtons={false}
         showsPagination={false}
-        containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
+        containerStyle={{
+          marginBottom: 30,
+          width: "100%",
+          height: SCREEN_HEIGHT / 4,
+        }}
       >
         {nowPlaying.map((movie) => (
           <Slide
@@ -83,6 +120,23 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
           />
         ))}
       </Swiper>
+      <ListTitle isDark={isDark}>Trending Movies</ListTitle>
+      <TrendingScroll
+        contentContainerStyle={{ paddingLeft: 30 }} // 스크롤뷰는 스타일을 줄 때 style prop 보다는 contentContainerStyle prop을 이용하여 주는게 좋다.
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      >
+        {trending.map((movie) => (
+          <Movie key={movie.id}>
+            <Poster path={movie.poster_path} />
+            <Title isDark={isDark}>
+              {movie.original_title.slice(0, 13)}
+              {movie.original_title.length > 13 ? "..." : null}
+            </Title>
+            <Votes isDark={isDark}>⭐️ {movie.vote_average}/10</Votes>
+          </Movie>
+        ))}
+      </TrendingScroll>
     </Container>
   );
 };
